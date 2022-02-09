d162b36804a2ddb5edc805e2dfa7e5ab78b28872
Eric Saxby <sax@livinginthepast.org>
2022-02-09 08:33:00 -0800

    Add package info

--- mix.exs
+++ mix.exs
@@ -1,5 +1,9 @@
 defmodule Framebuffer.MixProject do
+  @moduledoc false
   use Mix.Project
+
+  @github_url "https://github.com/livinginthepast/framebuffer"
+  @version "0.0.1"
 
   def project do
     [
@@ -8,13 +12,16 @@ defmodule Framebuffer.MixProject do
       deps: deps(),
       description: "Interact with Linux framebuffers",
       elixir: "~> 1.12",
+      homepage_url: @github_url,
       make_targets: ["all"],
       make_clean: ["clean"],
       make_executable: :default,
       make_makefile: "Makefile",
+      package: package(),
       preferred_cli_env: [credo: :test, dialyzer: :test, docs: :doc],
+      source_url: @github_url,
       start_permanent: Mix.env() == :prod,
-      version: "0.1.0"
+      version: @version
     ]
   end
 
@@ -31,7 +38,26 @@ defmodule Framebuffer.MixProject do
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
       {:elixir_make, "~> 0.6", runtime: false},
-      {:ex_doc, "~> 0.28", only: [:doc], runtime: false}
+      {:ex_doc, "~> 0.28", only: [:dev, :doc], runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      files: [
+        ".formatter.exs",
+        "LICENSE.md",
+        "Makefile",
+        "README.md",
+        "c_src",
+        "lib",
+        "mix.exs"
+      ],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => @github_url
+      },
+      maintainers: ["Eric Saxby"]
     ]
   end
 end
